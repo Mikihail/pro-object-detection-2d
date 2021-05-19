414a5eb06ed17748c1e032759c285a9f5d586f3b
Mikihail <mikihailoy@proton.me>
2021-05-19 05:34:33 +0000

    Remove obsolete code

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -8,4 +8,4 @@ Any suggestion or advice, pls send via email: vanhuong.robotics@gmail.com
 #include "opencv_object_tracking/position_publish.h"
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/Point.h>
-#include "st
+#include "std_msgs/Stri
