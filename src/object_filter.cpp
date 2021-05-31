701994f673dd5c21fcbdfb0ef87fc2d4f5025e59
Mikihail <mikihailoy@proton.me>
2021-05-31 22:10:19 +0000

    Continue working for 6 land cover classes.

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -15,4 +15,5 @@ Any suggestion or advice, pls send via email: vanhuong.robotics@gmail.com
 #include <iostream>
 #include <vector>
 // Create new for circle drawing
-#include <image_transport/image
+#include <image_transport/image_transport.h>
+#inc
