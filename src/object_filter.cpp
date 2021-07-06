31e70d70930654533165b1b72f491d75ff7fc85c
Mikihail <mikihailoy@proton.me>
2021-07-06 07:27:02 +0000

    Added code climate badge

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -29,4 +29,5 @@ float x_value, y_value, z_value;
 int x_position, y_position, z_position;
 //End of global variable.
 cv_bridge::CvImagePtr cv_ptr;
-sensor_msgs::PointC
+sensor_msgs::PointCloud2 my_pcl;
+usi
