c0c920d3fe13e31b41599a240f483a365f376e3a
Mikihail <mikihailoy@proton.me>
2021-07-19 12:16:48 +0000

    unnecessary cast

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -43,4 +43,5 @@ ros::Publisher *pub;
 const int FRAME_WIDTH = 640;
 const int FRAME_HEIGHT = 480;
 const int MAX_NUM_OBJECTS = 50;
-const int MIN_OBJECT_AREA
+const int MIN_OBJECT_AREA = 20 * 20;
+con
