55e7666b92f508c2686c04a26e16a6dad069bca1
Mikihail <mikihailoy@proton.me>
2021-08-04 17:55:41 +0000

    more placeholders

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -50,4 +50,6 @@ static const std::string OPENCV_WINDOW = "Image Window";
 static const std::string windowName1 = "HSV image";
 static const std::string windowName2 = "Thresholded Image";
 static const std::string windowName3 = "After Morphological Operations";
-static const std::string trackbarWindowName = "Tr
+static const std::string trackbarWindowName = "Track bars";
+
+voi
