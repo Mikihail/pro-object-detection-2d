7cd41fb74cda08fc7b743b9479d017b04c303b7d
Mikihail <mikihailoy@proton.me>
2021-08-27 21:10:44 +0000

    spec fixes

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -64,4 +64,4 @@ void createTrackbars()
         //Create window for trackbars
         namedWindow("Track_bars", 0);
         char TrackbarName[50];
-        createTrackbar("H_MIN", "Track_bars
+        createTrackbar("H_MIN", "Track_bars", &H_MIN, H_MAX
