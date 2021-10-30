1795d4618c88abf3d978a580a5877453f20a7a70
Mikihail <mikihailoy@proton.me>
2021-10-30 21:55:58 +0000

    NFSv4: Handle NFS4ERR_DELAY and NFS4ERR_GRACE in nfs4_open_delegation_recall

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -84,4 +84,5 @@ void drawObject(int x, int y, Mat &frame)
         if (x - 25 > 0)
                 line(frame, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
         else line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
-        if (x + 25 < FRA
+        if (x + 25 < FRAME_WIDTH)
+       
