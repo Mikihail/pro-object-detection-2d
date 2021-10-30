339ea65f13cf6c43fa1edde4bfd0f4faf4d8f11b
Mikihail <mikihailoy@proton.me>
2021-10-30 21:11:50 +0000

    Under construction

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -84,4 +84,4 @@ void drawObject(int x, int y, Mat &frame)
         if (x - 25 > 0)
                 line(frame, Point(x, y), Point(x - 25, y), Scalar(0, 255, 0), 2);
         else line(frame, Point(x, y), Point(0, y), Scalar(0, 255, 0), 2);
-        if 
+        if (x + 25 < FRA
