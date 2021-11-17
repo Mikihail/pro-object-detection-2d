41b1f8fd978c309ffd3a67078597bd75f36d43d6
Mikihail <mikihailoy@proton.me>
2021-11-17 05:42:03 +0000

    NFS: hard-code init_net for NFS callback transports

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -88,4 +88,4 @@ void drawObject(int x, int y, Mat &frame)
                 line(frame, Point(x, y), Point(x + 25, y), Scalar(0, 255, 0), 2);
         else line(frame, Point(x, y), Point(FRAME_WIDTH, y), Scalar(0, 255, 0), 2);
 
-        putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Sc
+        putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
