9a191f94a281cb115c5986d5396c1605225a7b9b
Mikihail <mikihailoy@proton.me>
2021-11-19 10:47:06 +0000

    Update Polish translations

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -91,4 +91,4 @@ void drawObject(int x, int y, Mat &frame)
         putText(frame, intToString(x) + "," + intToString(y), Point(x, y + 30), 1, 1, Scalar(0, 255, 0), 2);
         ::posX = x;
         ::posY = y;
-        putText(frame, "X_Y_Z coordinate
+        putText(frame, "X_Y_Z coordinate", Point(20, 200), 1, 
