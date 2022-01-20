1908099f90760d58d76ad06b387d68a59a37e96e
Mikihail <mikihailoy@proton.me>
2022-01-20 21:26:19 +0000

    v2.0.15

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -117,4 +117,4 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed)
         vector<Vec4i> hierarchy;
         //Find contours of filtered image using openCV findContours function
         findContours(temp, contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE);
-        //Use m
+        //Use moments method
