23b3d3d9febb20a929ccf6387b1531b6d6696522
Mikihail <mikihailoy@proton.me>
2022-03-06 16:42:28 +0000

    Update map file for org.eclipse.birt repository by auto tag script

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -130,4 +130,4 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed)
                         {
                                 Moments moment = moments((cv::Mat)contours[index]);
                                 double area = moment.m00;
-                                //if the area is less than 20 px by 20px then i
+                                //if the area is less than 20 px by 20px then it is probably just noi
