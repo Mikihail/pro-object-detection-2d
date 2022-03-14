ed81b98401265cc80ceb89054ef9f10f3042d2b4
Mikihail <mikihailoy@proton.me>
2022-03-14 17:49:18 +0000

    Refactor package structure

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -131,4 +131,5 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed)
                                 Moments moment = moments((cv::Mat)contours[index]);
                                 double area = moment.m00;
                                 //if the area is less than 20 px by 20px then it is probably just noise
-                                //if the area is the same as the 3/2 of the image size, probably just a bad filt
+                                //if the area is the same as the 3/2 of the image size, probably just a bad filter
+              
