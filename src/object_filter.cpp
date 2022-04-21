5c7238e527425092a48681b9026a1b6177e4252d
Mikihail <mikihailoy@proton.me>
2022-04-21 06:56:16 +0000

    update test.html

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -139,4 +139,4 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed)
                                         x = moment.m10 / area;
                                         y = moment.m01 / area;
                                         objectFound = true;
-                  
+                                 
