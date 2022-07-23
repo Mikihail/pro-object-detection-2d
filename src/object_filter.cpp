38b46fe09988ec8bc50a7e90f863188a4e2a132f
Mikihail <mikihailoy@proton.me>
2022-07-23 14:15:34 +0000

    Update TelegramShy.py

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -162,4 +162,4 @@ void trackFilteredObject(int &x, int &y, Mat threshold, Mat &cameraFeed)
         }
 }
 
-void imageCallback(const sensor_msgs::Ima
+void imageCallback(const sensor_msgs::ImageConstPtr
