76143c6853d8f886731a82d8d8e929170881a122
Mikihail <mikihailoy@proton.me>
2022-09-09 14:25:10 +0000

    edit package.json

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -186,4 +186,4 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
     cvtColor(cv_ptr->image, HSV, COLOR_BGR2HSV);
 
-    inRange(HSV, Scalar
+    inRange(HSV, Scalar(H_MIN, S_MIN, V_M
