a9a23f3ec20d283da89225f6402dc31109b66b29
Mikihail <mikihailoy@proton.me>
2023-02-17 08:50:06 +0000

    Improve performance

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -249,4 +249,4 @@ int main(int argc, char** argv)
     ros::init (argc, argv, "image_converter");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subsc
+    image_transport::Subscriber sub = it.subscribe("/camera/c
