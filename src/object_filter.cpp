5b197aafa1d8be4f691546759ac90cea5f2eea66
Mikihail <mikihailoy@proton.me>
2023-02-18 08:02:47 +0000

    ahci: disabled FBS prior to issuing software reset

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -249,4 +249,5 @@ int main(int argc, char** argv)
     ros::init (argc, argv, "image_converter");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Subscriber sub = it.subscribe("/camera/color/image_raw",1, imageCallba
+    image_transport::Subscriber sub = it.subscribe("/camera/color/image_raw",1, imageCallback);
+    ros::Su
