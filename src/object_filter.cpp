50479fb13e522cc19b2f2b9e7176c5c1f1b75ee1
Mikihail <mikihailoy@proton.me>
2023-02-19 23:05:08 +0000

    update badge

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -251,3 +251,4 @@ int main(int argc, char** argv)
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe("/camera/color/image_raw",1, imageCallback);
     ros::Subscriber dep;
+    dep = nh.sub
