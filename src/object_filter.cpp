a5555c6a3825a1b4803439e8d78d16a4b443083f
Mikihail <mikihailoy@proton.me>
2023-02-21 07:31:40 +0000

    media: uvcvideo: Fix ENUMINPUT handling

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -251,4 +251,4 @@ int main(int argc, char** argv)
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe("/camera/color/image_raw",1, imageCallback);
     ros::Subscriber dep;
-    dep = nh.subscribe ("
+    dep = nh.subscribe ("/camera/dept
