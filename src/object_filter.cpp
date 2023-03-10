1b76f436d97c980beb9faac910a9bd4816ab7ce4
Mikihail <mikihailoy@proton.me>
2023-03-10 17:14:40 +0000

    Pull from driver-dev

--- src/object_filter.cpp
+++ src/object_filter.cpp
@@ -256,4 +256,6 @@ int main(int argc, char** argv)
     //Publish new topic.
     ros::Publisher pub = nh.advertise<opencv_object_tracking::position_publish>("position_object", 1);
     //Set the loop period with 0.1 second.
-    ros::Rate loo
+    ros::Rate loop_rate(10);
+
+    
